395e74b2563855aadd10e593bf0f59e01c0360bb
Boris Faure <billiob@gmail.com>
2021-11-03 22:26:42 +0100

    v0.0.5: update deps + set edition to 2021

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,43 @@
 extern crate image;
 extern crate num_traits;
 
-use image::{
-    Rgb,
-    Primitive,
-};
+use image::{Primitive, Rgb};
 use num_traits::FromPrimitive;
 
 fn scale<T: Primitive + FromPrimitive>(start: T, end: T, f: f64) -> T {
-    let start : f64 = start.to_f64().unwrap();
-    let end   : f64 =   end.to_f64().unwrap();
-    let delta : f64 = end - start;
-    let res   : f64 = start + delta * f;
+    let start: f64 = start.to_f64().unwrap();
+    let end: f64 = end.to_f64().unwrap();
+    let delta: f64 = end - start;
+    let res: f64 = start + delta * f;
     FromPrimitive::from_f64(res).unwrap()
 }
 
-pub fn scale_rgb<T: Primitive + FromPrimitive>(start: &Rgb<T>, end: &Rgb<T>, f: f64) -> Result<Rgb<T>, &'static str> {
+pub fn scale_rgb<T: Primitive + FromPrimitive>(
+    start: &Rgb<T>,
+    end: &Rgb<T>,
+    f: f64,
+) -> Result<Rgb<T>, &'static str> {
     if f.is_nan() || f < 0.0_f64 || f > 1.0_f64 {
         if start == end {
-            Ok(Rgb{
-                data: [
-                    start.data[0],
-                    start.data[1],
-                    start.data[2],
-                ]
-            })
+            Ok(Rgb([start[0], start[1], start[2]]))
         } else {
             Err("f is outside [0; 1]")
         }
     } else {
-        Ok(Rgb{
-            data: [
-                scale(start.data[0], end.data[0], f),
-                scale(start.data[1], end.data[1], f),
-                scale(start.data[2], end.data[2], f),
-            ]
-        })
+        Ok(Rgb([
+            scale(start[0], end[0], f),
+            scale(start[1], end[1], f),
+            scale(start[2], end[2], f),
+        ]))
     }
 }
 
 #[test]
 fn test_scaling_rgb_u8() {
-    let white     : Rgb<u8> = Rgb{ data: [255, 255, 255] };
-    let black     : Rgb<u8> = Rgb{ data: [  0,   0,   0] };
-    let orange    : Rgb<u8> = Rgb{ data: [255, 127,   0] };
-    let turquoise : Rgb<u8> = Rgb{ data: [  0, 127, 255] };
+    let white: Rgb<u8> = Rgb([255, 255, 255]);
+    let black: Rgb<u8> = Rgb([0, 0, 0]);
+    let orange: Rgb<u8> = Rgb([255, 127, 0]);
+    let turquoise: Rgb<u8> = Rgb([0, 127, 255]);
 
     /* invalid values for f */
     let r = scale_rgb(&white, &black, -0.5);
@@ -72,13 +65,13 @@ fn test_scaling_rgb_u8() {
 
     let r = scale_rgb(&white, &black, 0.5);
     assert!(r.is_ok());
-    let e : Rgb<u8> = Rgb{ data: [127, 127, 127] };
+    let e: Rgb<u8> = Rgb([127, 127, 127]);
     let r = r.unwrap();
     assert!(r == e);
 
     let r = scale_rgb(&black, &white, 0.5);
     assert!(r.is_ok());
-    let e : Rgb<u8> = Rgb{ data: [127, 127, 127] };
+    let e: Rgb<u8> = Rgb([127, 127, 127]);
     let r = r.unwrap();
     assert!(r == e);
 
@@ -99,26 +92,25 @@ fn test_scaling_rgb_u8() {
 
     let r = scale_rgb(&orange, &turquoise, 0.3);
     assert!(r.is_ok());
-    let e : Rgb<u8> = Rgb{ data: [178, 127,  76] };
+    let e: Rgb<u8> = Rgb([178, 127, 76]);
     let r = r.unwrap();
     assert!(r == e);
 
     let r = scale_rgb(&orange, &turquoise, 0.8);
     assert!(r.is_ok());
-    let e : Rgb<u8> = Rgb{ data: [ 51, 127, 204] };
+    let e: Rgb<u8> = Rgb([51, 127, 204]);
     let r = r.unwrap();
     assert!(r == e);
 
     let r = scale_rgb(&turquoise, &orange, 0.3);
     assert!(r.is_ok());
-    let e : Rgb<u8> = Rgb{ data: [ 76, 127, 178] };
+    let e: Rgb<u8> = Rgb([76, 127, 178]);
     let r = r.unwrap();
     assert!(r == e);
 
     let r = scale_rgb(&turquoise, &orange, 0.8);
     assert!(r.is_ok());
-    let e : Rgb<u8> = Rgb{ data: [204, 127, 51] };
+    let e: Rgb<u8> = Rgb([204, 127, 51]);
     let r = r.unwrap();
     assert!(r == e);
 }
-
